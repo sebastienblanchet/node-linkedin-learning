332b4dee6dbbf5adc43346f842c2880a307aee66
Seb Blanchet <s3blanch@edu.uwaterloo.ca>
2020-03-29 17:32:29 -0400

    Post methods

--- src/server.js
+++ src/server.js
@@ -1,23 +1,33 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 var messages = [{
-        name: 'Tim',
-        message: 'Hi'
-    },
-    {
-        name: 'Jane',
-        message: 'Hello'
-    }
+    name: 'Tim',
+    message: 'Hi'
+  },
+  {
+    name: 'Jane',
+    message: 'Hello'
+  }
+
 ]
 
-// basic definition of a get route
 app.get('/messages', (req, res) => {
-    res.send(messages)
+  res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+  messages.push(req.body)
+  res.sendStatus(200)
 })
 
 var server = app.listen(3000, () => {
-    console.log('server is listening on port', server.address().port)
+  console.log('server is listening on port', server.address().port)
 })
